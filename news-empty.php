e0500c9efcc28296f34c7b7f19a82a9d2236c3b8
Roman Pohorecki <romanp@gmail.com>
2016-11-26 18:57:27 -0800

    added empty content tile for News & Schedule pages

---
+++ news-empty.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <title>Seattle Polish Film Festival</title>
+	    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:200, 300,400,600,700,800' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet'>
+
+		<link type="text/css" rel="stylesheet" href="css/style.css" media="screen" />
+		<script src="https://code.jquery.com/jquery-latest.js"></script>
+		<script src="js/full-screen-video.js" type="text/javascript"></script>
+		<script src="js/menu-fade-in.js" type="text/javascript"></script>
+		<script src="js/scroll-to-top.js" type="text/javascript"></script>
+		
+	</head>
+	<body id="news">
+
+		<?php include 'includes/nav.php';?>
+
+		<section id='splash' style="background-image: url('images/news/news-header.jpg')">
+			<div class='section-black-overlay'>
+				<h1 class='vertical-align'>News</h1>
+			</div>
+
+			<div class='triangle triangle-lower-left '>
+				<img src='images/site/triangle.png'>
+			</div>
+		</section>
+
+		<section>
+			<h2>2016 news</h2>
+
+			<div class='tile content-empty'>
+				<p>There's No news here</p>
+				<span>take a look at the other years...</span>
+			</div>
+
+			<div class='tile content-empty'>
+				<p>There aren't any events here</p>
+				<span>take a look at the other years...</span>
+			</div>
+
+			<div class='button-flex'>
+				<a class='button-3' href=''>Previous Year</a>
+				<a class='button-3' href=''>Next Year</a>
+			</div>
+
+		</section>
+
+		
+		<?php include 'includes/footer.php';?>
+
+	</body>
+</html>		
