31701178be68a397193e4b8784b6f13e969cfd59
Roman Pohorecki <romanp@gmail.com>
2016-11-26 18:56:42 -0800

    404 error page added

---
+++ 404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <title>Seattle Polish Film Festival</title>
+	    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:200,300,400,600,700,800' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet'>
+
+		<link type="text/css" rel="stylesheet" href="css/style.css" media="screen" />
+		<script src="https://code.jquery.com/jquery-latest.js"></script>
+		<script src="js/full-screen-video.js" type="text/javascript"></script>
+		<script src="js/menu-fade-in.js" type="text/javascript"></script>
+		<script src="js/scroll-to-top.js" type="text/javascript"></script>
+		
+	</head>
+	<body>
+		<?php include 'includes/nav.php';?>
+
+		<div id="error-404">
+			<section class='section-black-overlay'>
+			<div class='vertical-align'>
+				<h2>404</h2>
+				<h1>The page you're looking <br> for isn't here.</h1>
+				<br>
+				<p>If you're experiencing a problem<br> with the site, please <a href='mailto:michal@polishfilms.org'>email us.</a>
+			</div>
+			</section>
+		</div>
+
+	</body>
+</html>		
